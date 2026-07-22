b67cea99b9667e5b065a8d6fc82cdc296459e9a8
Rosa <rosearysh@gmail.com>
2026-07-22 16:04:03 +0330

    created general templates folder
    yuuhoooo
    I forgot to add that template folder beside the manage.py file

--- website/urls.py
+++ website/urls.py
@@ -4,5 +4,5 @@ from website.views import *
 urlpatterns = [
     path('', index_view),
     path('about', about_view),
-    path('contacts', contact_view)
+    path('contact', contact_view)
 ]
--- website/views.py
+++ website/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
 
 def index_view(request):
     return render(request, 'website/index.html')
 
 def contact_view(request):
-    return render(request, 'website/contacts.html')
+    return render(request, 'website/contact.html')
 
 def about_view(request):
     return render(request, 'website/about.html')
