bc766fcc3d1b0fc9a493b07db49a495e2f5c58f1
Rosa <rosearysh@gmail.com>
2026-07-29 17:48:50 +0330

    I did it all just for test

--- website/urls.py
+++ website/urls.py
@@ -4,7 +4,8 @@ from website.views import *
 app_name = 'website'
 
 urlpatterns = [
-    path('', index_view,name='index'),
-    path('about', about_view,name='about'),
-    path('contact', contact_view,name='contact')
+    path('', index_view, name='index'),
+    path('about', about_view, name='about'),
+    path('contact', contact_view, name='contact'),
+    path('test', test_view, name='test')
 ]
--- website/views.py
+++ website/views.py
@@ -8,3 +8,6 @@ def contact_view(request):
 
 def about_view(request):
     return render(request, 'website/about.html')
+
+def test_view(request):
+    return render(request, 'website/test.html',{'name':'rose', 'lastname':'arayesh'})
