c3551a4109c9e4b3786614afc2c08034f7fa0e7c
Rosa <rosearysh@gmail.com>
2026-08-01 03:02:30 +0330

    Customize Post admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -3,5 +3,13 @@ from blog.models import Post
 
 #@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    pass
+    date_hierarchy = 'created_date'
+    empty_value_display = 'Not set'
+    #fields = ('title',)
+    #exclude = ('content',)
+    list_display = ('id','title','counted_views','status','published_date','created_date')
+    list_filter = ('status',)
+    ordering = ['-created_date']
+    search_fields = ['title','content']
+    
 admin.site.register(Post,PostAdmin)
