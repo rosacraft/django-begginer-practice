83d3325c8d7c0e76e83525ebe0149665cd4c555f
Rosa <rosearysh@gmail.com>
2026-08-01 02:20:23 +0330

    Update Django admin configuration
    - Changed project timezone settings
    - Added Post model to Django admin panel
    - Customized object representation in admin panel
    - Added db.sqbpro to .gitignore

--- blog/models.py
+++ blog/models.py
@@ -10,7 +10,11 @@ class Post(models.Model):
     # tag
     # category
     counted_views = models.IntegerField(default=0)
-    staus = models.BooleanField(default=False)
+    status = models.BooleanField(default=False)
     published_date = models.DateTimeField(null=True)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
+    
+    
+    def __str__(self):
+        return f"{self.id} - {self.title}"
