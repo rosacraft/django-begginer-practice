7afeb7123271fd5a3ea191310214a716f57ac472
Rosa <rosearysh@gmail.com>
2026-08-04 22:56:33 +0330

    finish adding popular posts widget using inclusion template tag
    make a small change to css file

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -3,12 +3,29 @@ from blog.models import Post
 
 register = template.Library()
 
+#  ---Tests fun---
 @register.simple_tag(name='totalposts')
 def fun ():
-    posts = Post.objects.filter(status=0).count()
+    posts = Post.objects.filter(status=1).count()
     return posts
 
 @register.simple_tag(name='posts')
 def fun ():
     posts = Post.objects.filter(status=1)
     return posts
+
+@register.filter
+def snippet(value,arg=20):
+    return value[:arg] + '...'
+
+@register.inclusion_tag('popularposts.html')
+def popularposts():
+    posts = Post.objects.filter(status=1).order_by('published_date')
+    return {'posts': posts}
+
+
+# ---Main fun---
+@register.inclusion_tag('blog/blog-latest-posts.html')
+def latestposts(arg = 3):
+    posts = Post.objects.filter(status=1).order_by('published_date')[:arg]
+    return {'posts': posts}
