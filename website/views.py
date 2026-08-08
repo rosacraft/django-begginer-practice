3c9c7c7fafa2e904fcbcf771d704d70375adbf9e
Rosa <rosearysh@gmail.com>
2026-08-08 16:59:05 +0330

    test using Model Form

--- website/views.py
+++ website/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from website.models import Contact
+from website.forms import NameForm, ContactForm
+from django.http import HttpResponse
 
 def index_view(request):
     return render(request, 'website/index.html')
@@ -12,20 +14,14 @@ def about_view(request):
 
 def test_view(request):
     if request.method == 'POST':
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        subject = request.POST.get('subject')
-        message = request.POST.get('message')
-        c = Contact()
-        c.name = name
-        c.email = email
-        c.subject = subject
-        c.message = message
-        c.save()
         
-        print(name, email, subject, message)
-        
-    elif request.method == 'GET':
-        print('get')
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('done')
+        else:
+            return HttpResponse('not valid')
+            
+    form = ContactForm()
     
-    return render(request, 'test.html', {})
+    return render(request, 'test.html', {'form': form})
